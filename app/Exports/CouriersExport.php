3458981dc0cf37db781af16d5a9e091646f4472a
Sivaji Meenugu <shivajimeenugu@gmail.com>
2022-10-04 15:30:40 +0530

    modified excel format

--- app/Exports/CouriersExport.php
+++ app/Exports/CouriersExport.php
@@ -8,8 +8,10 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-
-
+use Illuminate\Support\Facades\DB;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Color;
+use \PhpOffice\PhpSpreadsheet\Style\Alignment;
 class CouriersExport implements FromCollection,WithHeadings,ShouldAutoSize,WithStyles
 {
     /**
@@ -24,14 +26,33 @@ class CouriersExport implements FromCollection,WithHeadings,ShouldAutoSize,WithS
 
     public function styles(Worksheet $sheet)
     {
+
+
+        $data=DB::select(DB::raw('
+        SELECT
+            cid,
+            cdate,
+            concat(cfrom," To ",cto) AS PARTICULARS,
+            amount
+        FROM
+            couries
+        WHERE
+            billid='.$this->bid.'
+        '));
+
+        $count=count($data);
+        $sheet->getStyle('A1:D'.($count+1))->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN)->setColor(new Color('00000000'));
+        $sheet->getStyle('A1:D'.($count+1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('A1:D'.($count+1))->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
+
         return [
             // Style the first row as bold text.
             1    => ['font' => ['bold' => true ,'color' => ['argb'=>'FFFF0000']]],
             // 1 => ['font' => ['color'=>'#FF0000']],
             // 1 => ['fill' => ['color'=>'FFFF0000']],
-
+            'A1:D'.($count+1) =>['borders'=>['allborders'=>true]]
             // // Styling a specific cell by coordinate.
-            // 'B2' => ['font' => ['italic' => true]],
+            // 'B2' => ['font' => ['italic' => true]]
 
             // // Styling an entire column.
             // 'C'  => ['font' => ['size' => 16]],
@@ -41,19 +62,28 @@ class CouriersExport implements FromCollection,WithHeadings,ShouldAutoSize,WithS
     public function headings(): array
     {
         return [
-            'SI.NO',
-            'ID',
-            'Date',
-            'Amount',
-            'From',
-            'To',
+            'COURIER ID',
+            'COURIER DATE',
+            'PARTICULARS',
+            'AMOUNT',
         ];
     }
 
     public function collection()
     {
-        return couries::select('id', 'cid', 'cdate','amount','cfrom','cto')
-        ->where('billid',$this->bid)
-        ->get();
+
+        $data=DB::select(DB::raw('
+        SELECT
+            cid,
+            cdate,
+            concat(cfrom," TO ",cto) AS PARTICULARS,
+            amount
+        FROM
+            couries
+        WHERE
+            billid='.$this->bid.'
+        '));
+        // dd($data);
+        return collect( $data);
     }
 }
